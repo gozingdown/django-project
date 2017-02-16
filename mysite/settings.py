a0de2ea35a196b5ad17be5eea1a745dc53f56044
gozingdown <gongzhenggz@gmail.com>
2017-02-16 21:59:29 +0000

    add view

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME' : 'webdb',
         'USER' : 'root',
-        'PASSWORD' : '891024',
+        'PASSWORD' : 'Today123@GZ',
         'HOST':'127.0.0.1',
         'PORT':'3306',
         'default-character-set' : 'utf8',
--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,14 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello - polls.index")
-	
+
+def detail(request, question_id):
+	 return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+	response = "You're looking at the results of question %s" % question_id
+	return HttpResponse(response)
+
+def vote(request, question_id):
+	response = "You are voting on question %s" % question_id
+	return HttpResponse(response)

46145eaf63bb954c13f22994ba4051027aae0556
gozingdown <gongzhenggz@gmail.com>
2017-02-16 23:53:10 -0500

    update index() to do db lookup

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,20 @@
 from django.http import HttpResponse
+from polls.models import Question
+
 
 def index(request):
-	return HttpResponse("Hello - polls.index")
+    # https://docs.djangoproject.com/en/1.10/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
-	 return HttpResponse("You're looking at question %s." % question_id)
+     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s" % question_id
-	return HttpResponse(response)
+    response = "You're looking at the results of question %s" % question_id
+    return HttpResponse(response)
 
 def vote(request, question_id):
-	response = "You are voting on question %s" % question_id
-	return HttpResponse(response)
+    response = "You are voting on question %s" % question_id
+    return HttpResponse(response)

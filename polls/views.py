c2f1f9e07838d8d482e9192bf2c0ebf95c76bee5
gozingdown <gongzhenggz@gmail.com>
2017-02-19 21:52:48 -0500

    tutorial-part5,6 add tests and styles

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+#http://stackoverflow.com/questions/38944378/django-from-django-urls-import-reverse-importerror-no-module-named-urls
+from django.core.urlresolvers import reverse
 from django.views import generic
 
 from polls.models import Choice, Question
+
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -13,12 +16,18 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+        ).order_by('-pub_date')[:5]
+        #'-' indicates descending order
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):

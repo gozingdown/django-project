4bdfb3ad1e02c686fd5301d003d37b0f56ec9c81
gozingdown <gongzhenggz@gmail.com>
2017-02-22 22:50:19 -0500

    Tutorial - making queries

--- places/models.py
+++ places/models.py
@@ -59,3 +59,35 @@ s. Thus you have to create a new related_name for p1 to refer to it's suppliers.
 p1.supplier 
 
 '''
+##################
+#Making Queries
+#https://docs.djangoproject.com/en/1.10/topics/db/queries/
+##################
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.headline

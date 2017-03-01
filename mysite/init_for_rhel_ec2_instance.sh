eae1ada5ae79f9d7d989caa799ee92cf0663d32b
gozingdown <gongzhenggz@gmail.com>
2017-03-01 19:54:45 +0000

    reorganize init script

---
+++ mysite/init_for_rhel_ec2_instance.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+install_pip() {
+    if [[ `pip --version` == *"site-packages"* ]]; then
+        echo '[ALREADY_INSTALLED]pip already installed' >&1
+        return 0
+    fi
+    #install wget
+    if [ "`rpm -qa | grep setuptools`" = "" ]; then
+        yum -y install wget
+    fi
+    #install python setuptools
+    if [ "`rpm -qa | grep python-setuptools`" = "" ]; then
+        yum -y install python-setuptools
+        if [ `rpm -qa | grep python-setuptools` = "" ]; then
+            echo '[FAILURE]setuptools is not installed' >&2
+            exit 1
+        else
+            echo '[SUCCESS]setuptools installed successfully' >&1
+        fi
+    fi
+    
+    wget https://bootstrap.pypa.io/get-pip.py
+
+    python get-pip.py
+
+    rm get-pip.py
+
+    if [[ ! `pip --version` == *"site-packages"* ]]; then
+        echo '[FAILURE]fail to install pip' >&2
+        exit 1
+    else
+        echo '[SUCCESS]pip installed successfully' >&1
+    fi
+
+    pip --version
+}
+
+install_git() {
+    if [[ `git --version` == *"git version"* ]]; then
+        echo '[ALREADY_INSTALLED]Git already installed' >&1
+        return 0
+    fi
+    yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel
+    yum -y install gcc perl-ExtUtils-MakeMaker
+
+    cd /usr/src
+    wget https://www.kernel.org/pub/software/scm/git/git-2.12.0.tar.gz
+    tar xzf git-2.12.0.tar.gz
+
+    cd git-2.12.0
+    make prefix=/usr/local/git all
+    make prefix=/usr/local/git install
+
+    export PATH=$PATH:/usr/local/git/bin
+    source /etc/bashrc
+
+    git --version
+
+    if [[ ! `git --version` == *"git version"* ]]; then
+        echo '[FAILURE]fail to install Git' >&2
+        exit 1
+    else
+        echo '[SUCCESS]Git installed successfully' >&1
+    fi
+}
+
+##################################start initializing
+
+if [ "`rpm -qa | grep vim`" = "" ]; then
+    yum -y install vim
+fi
+
+if [ "`rpm -qa | grep python-setuptools`" = "" ]; then
+    yum -y install wget
+fi
+
+#install pip - start
+install_pip
+
+#install Git
+install_git
+
+##################################end of initializing

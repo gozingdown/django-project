d13393f6316563f0e5b3aebc48705c6d6dcfd441
gozingdown <gongzhenggz@gmail.com>
2017-03-20 23:12:23 -0400

    django doc - form : different form template techniques

--- persons/forms.py
+++ persons/forms.py
@@ -6,5 +6,5 @@ class NameForm(forms.Form):
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length = 100, initial ='initial text')#http://stackoverflow.com/questions/604266/django-set-default-form-values
     message = forms.CharField(widget = forms.Textarea)
-    sender = forms.EmailField()
+    sender = forms.EmailField(help_text='A valid email address, please.')
     cc_myself = forms.BooleanField(required=False)
